d1c76360df6d10ca0828f7daf204273e6f5ce587
Maggie Liu <maggie.j.liu@gmail.com>
2023-07-16 22:09:27 -0700

    add redirect to registration form

--- site/next.config.js
+++ site/next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/register',
+        destination: 'https://forms.gle/ZvhFctVAAN5W5YaP9',
+        permanent: false,
+      },
+    ]
+  },
+}
 
 module.exports = nextConfig
