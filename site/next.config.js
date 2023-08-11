577b69ed10837c996122b76454a3f1ccd5620e4e
Maggie Liu <maggie.j.liu@gmail.com>
2023-08-11 01:45:23 -0700

    add schedule redirect

--- site/next.config.js
+++ site/next.config.js
@@ -7,6 +7,11 @@ const nextConfig = {
         destination: 'https://docs.google.com/forms/d/e/1FAIpQLSfatAsK7zWhWRGjxoAhOddJrfd3x23r69sCx7htvBqtx5F9zw/viewform',
         permanent: false,
       },
+      {
+        source: "/schedule",
+        destination: "https://docs.google.com/document/d/1QX3vVyNkFQQUR7vu_g6CtszUA-7RPDjHbd-tUW6tALo/edit",
+        permanent: false
+      }
     ]
   },
 }
